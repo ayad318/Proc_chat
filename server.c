cb7ea78fe918e763cc10526f39ea82eb094846cb
Ayad Farhat <ayadfarhat@Ayads-MacBook-Pro.local>
2021-05-09 11:45:48 +0300

    hello

--- server.c
+++ server.c
@@ -21,18 +21,18 @@ int main(int argc, char** argv) {
     if(-1 == fd){
         fprintf(stderr, "Unable to open gevent");
     }
-    FILE* read_channel = fopen(fd, "r");
+    FILE* read_channel = fdopen(fd, "r");
     //read from gevent and store and check error
     char buf[BUF_SZ];
     fgets(buf,BUF_SZ,read_channel);
     fclose(read_channel);
     /*if(read(fd,buf,BUF_SZ) == -1){
         fprintf(stderr, "Unable to read gevent");
-    }*/
+    }
     
     if (close(fd) == -1){
         fprintf(stderr, "Unable to close gevent");
-    }
+    }*/
     
     return 0;
 }
