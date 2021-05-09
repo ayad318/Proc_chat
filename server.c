dad80974ed52820a394de670650d16484e53608c
Ayad Farhat <ayadfarhat@Ayads-MacBook-Pro.local>
2021-05-09 12:53:37 +0300

    hello

--- server.c
+++ server.c
@@ -21,16 +21,13 @@ int main(int argc, char** argv) {
     //open gevent for read only and check error
 
 
-    int fd = open(CHANNEL_NAME, O_RDONLY);
-    lseek(fd, 0, SEEK_SET);
+    FILE* read_channel = fopen(CHANNEL_NAME, "r");
     char buf[BUF_SZ];
-    read(fd, buf, BUF_SZ);
-    
 
-    //while(1){
-    //   fgets(buf,BUF_SZ,read_channel);
-    //}
+    while(1){
+        fgets(buf,BUF_SZ,read_channel);
+    }
     // close channel
-    close(fd);
+    fclose(read_channel);
     return 0;
 }
