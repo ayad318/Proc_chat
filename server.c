57d2912c05335d69cf944dda6ff5f8373eccf7dd
Ayad Farhat <ayadfarhat@Ayads-MacBook-Pro.local>
2021-05-09 11:26:42 +0300

    hello

--- server.c
+++ server.c
@@ -2,31 +2,36 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include<fcntl.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <string.h>
 
-#define BUF_SZ (256)
+#define BUF_SZ (2048)
 #define CHANNEL_NAME ("gevent")
 
 int main(int argc, char** argv) {
-    if((mkfifo(CHANNEL_NAME, S_IRWXU | S_IRWXG)) >= 0){
-        int fd = open(CHANNEL_NAME, O_RDONLY);
-        if(fd > 0){
-            FILE* read_channel = fopen(fd, "r");
-            char buf[BUF_SZ];
-            while(fgets(buf, BUF_SZ, read_channel) != NULL) {
-                puts(buf);
-            }
-            if (fclose(fd) < 0){
-                fprintf(stderr, "Unable to close gevent");
-            } 
-        }else{
-            fprintf(stderr, "Unable to open gevent");
-            
-        }
-    }else  {
-        fprintf(stderr, "Unable to open pipe");
+    int gevent[2];
+    
+    // create gevent and check for error
+    if((mkfifo(CHANNEL_NAME, S_IRWXU | S_IRWXG)) == -1){
+        fprintf(stderr, "Unable to create gevent"); 
     }
+    //open gevent for read only and check error
+    int fd = open(CHANNEL_NAME, O_RDONLY);
+    if(-1 == fd){
+        fprintf(stderr, "Unable to open gevent");
+    }
+    
+    //read from gevent and store and check error
+    unsigned char buf[BUF_SZ];
+    if(read(fd,buf,BUF_SZ) == -1){
+        fprintf(stderr, "Unable to read gevent");
+    }
+    
+    if (close(fd) == -1){
+        fprintf(stderr, "Unable to close gevent");
+    } 
+    
     return 0;
 }
