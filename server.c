6b5a7b63fe1f9f9531ae2c009a21cfc2e7555871
Ayad Farhat <ayadfarhat@Ayads-MacBook-Pro.local>
2021-05-09 14:14:40 +0300

    hello

--- server.c
+++ server.c
@@ -26,46 +26,46 @@ void print_hex(const char *s)
 
 int main(int argc, char** argv) {
     
-    // create gevent and check for error
-    if((mkfifo(CHANNEL_NAME, S_IRWXU | S_IRWXG)) == -1){
-        fprintf(stderr, "Unable to create gevent"); 
-    }
-    //open gevent for read only and check error
-    int fd = open("gevent", O_RDONLY);
-    if(fd < 0){
-        fprintf(stderr, "Unable to open gevent");
-    }
+  // create gevent and check for error
+  if((mkfifo(CHANNEL_NAME, S_IRWXU | S_IRWXG)) == -1){
+      fprintf(stderr, "Unable to create gevent"); 
+  }
+  //open gevent for read only and check error
+  int fd = open("gevent", O_RDONLY);
+  if(fd < 0){
+      fprintf(stderr, "Unable to open gevent");
+  }
 
-    //read from gevent and check fro errors
-    char buf[BUF_SZ+1];
-    while(1){
-      size_t nread = read(fd,buf,BUF_SZ);
-      if (-1 == nread) {
-			  if (errno == EAGAIN) {
-				  printf("stop the blocking call to read\n");
-			  } else {
-				  perror("something wrong");
-			  }
+  //read from gevent and check fro errors
+  char buf[BUF_SZ+1];
+  while(1){
+    size_t nread = read(fd,buf,BUF_SZ);
+    if (-1 == nread) {
+	    if (errno == EAGAIN) {
+			  printf("stop the blocking call to read\n");
+		  } else {
+			  perror("something wrong");
+			}
 
+		  sleep(1);
+		  continue;
+    }else if (0 == nread) {
+      if (errno == EAGAIN) {
+			  printf("stop the blocking call to read\n");
 			  sleep(1);
 			  continue;
-      }else if (0 == nread) {
-			  if (errno == EAGAIN) {
-				  printf("stop the blocking call to read\n");
-				  sleep(1);
-				  continue;
-			  } else {
-				  // end of file?
-			  }
-		  } else {
-			  buf[BUF_SZ] = '\0';
-			  printf("nread: %zd\n", nread);
-			  printf("buffer: %s\n", buf);
+			} else {
+			  // end of file?
 		  }
+	  } else {
+      buf[BUF_SZ] = '\0';
+	    printf("nread: %zd\n", nread);
+		  printf("buffer: %s\n", buf);
+	  }
         
-    }
+  }
     
     // close channel
-    close(fd);
-    return 0;
+  close(fd);
+  return 0;
 }
