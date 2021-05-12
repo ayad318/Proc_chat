86aa0632b4063d50f40b984256027c6d06860cbc
Ayad Farhat <ayadfarhat@Ayads-MacBook-Pro.local>
2021-05-12 16:27:32 +0300

    hello

--- server.c
+++ server.c
@@ -204,8 +204,8 @@ int main(int argc, char** argv) {
 										
 									}else{
 										//check if _RD and write to it
-										filename_sz = strlen(ent->d_name);
-										fprintf(stderr,"filename size: %zu",filename_sz);
+										filename_sz = strlen(identifer);
+										fprintf(stderr,"filename size: %zu\n",filename_sz);
 										if(strcmp(ent->d_name + filename_sz - 3 ,RD_POSTFIX) == 0){
 											//open FIFO and write to it
 											sprintf(write_path,"%s/%s",domain,ent->d_name);
